eeec48aeb650ad48b0d754138d36717c6be96608
syedamariarasheed <syedamariarasheed@gmail.com>
2019-07-12 01:15:31 -0700

    Commit

---
+++ Typechecking/ConsoleApp2/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Recognition;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Text.RegularExpressions;
+
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+            SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-us"));
+            recognizer.SetInputToDefaultAudioDevice();
+            recognizer.LoadGrammar(new DictationGrammar());
+            recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_recognized);
+            while (true)
+            {
+                Console.ReadKey();
+            }*/
+
+            // Pattern to match identifier
+            string[] forloop ={ "keyword","opening round bracket","datatype","identifier","Equal","number","semi colon", "identifier", "relational operator","number","semi colon","identifier",
+                "Increment","closing round bracket" };
+            string[] intialization = { "datatype", "identifier", "Equal", "number", "semi colon" };
+
+            string pattern = @"^[A-Z|a-z]+[0-9|A-Z|a-z]*";
+            Regex reg2 = new Regex(pattern);
+            int total = 0;
+            string line = string.Empty;
+            string inputval = string.Empty;
+        label:
+            SpeechSynthesizer reader = new SpeechSynthesizer();
+            line = Console.ReadLine();
+
+            reader.SpeakAsync(line);
+
+            if (line != string.Empty)
+            {
+                inputval += line;
+                goto label;
+            }
+
+            string[] tokentype = new string[10000];
+            string[] previous = new string[1000];
+            string[] final = inputval.Split(' ');
+            int number;
+            bool find = false;
+
+
+
+            for (int i = 0; i < final.Length; i++)
+            {
+                find = false;
+                dictionary dictionary = new dictionary();
+                foreach (var input in dictionary.dictionary1)
+                {
+                    if (input.Key == final[i])
+                    {
+                        tokentype[i] = input.Value;
+                        total++;
+                        find = true;
+                        Console.WriteLine("Token # {2} < {0}, {1} >", final[i], input.Value, total);
+                    }
+                }
+
+                if (find != false)
+                { continue; }
+                if (int.TryParse(final[i], out number))
+                {
+                    tokentype[i] = "number";
+                    total++;
+                    Console.WriteLine("Token # {1} < {0}, number >", final[i], total);
+                }
+                else if (reg2.IsMatch(final[i]))
+                {
+                    tokentype[i] = "identifier";
+                    total++;
+                    Console.WriteLine("Token # {1} < {0}, identifier >", final[i], total);
+                }
+              
+            }
+            Console.WriteLine("Compiled Successfully---\n");
+            Program p = new Program();
+            p.StructureCheck(tokentype, forloop, intialization, final);
+            Console.ReadKey();
+
+        }
+
+        void StructureCheck(string[] tokentype, string[] forloop, string[] intialization, string[] final)
+        {
+           
+            bool ErrorCommand = false;
+            int i = 0;
+            int exist = 0;
+
+
+            //initialization
+            if (tokentype[0] == intialization[0])
+            {
+                ErrorCommand = true;
+                for (i = 0; i < tokentype.Length; i++)
+                {
+                    if (tokentype[i] == "datatype")
+                    {
+                    newvar:
+                        i++;
+
+                        if (tokentype[i] == "identifier")
+                        {
+                            i++;
+
+                            if (tokentype[i] == "Equal")
+                            {
+                                i++;
+
+                                if (tokentype[i] == "number" || tokentype[i] == "String Literal")
+                                {
+                                    i++;
+
+                                    if (tokentype[i] == "comma")
+                                    {
+                                        goto newvar;
+                                    }
+
+
+                                    if (tokentype[i] == "semi colon")
+                                    {
+                                        ErrorCommand = false;
+                                        Console.WriteLine("Sytax is correct");
+                                        break;
+                                    }
+                                }
+
+                            }
+                            if (tokentype[i] == "semi colon")
+                            {
+                                ErrorCommand = false;
+                                Console.WriteLine("Sytax is correct");
+                                break;
+                            }
+                        }
+
+                        if (tokentype[i] == "semi colon")
+                        {
+                            ErrorCommand = false;
+                            Console.WriteLine("Sytax is correct");
+                            break;
+                        }
+                    }
+
+                }
+                if (ErrorCommand == true)
+                {
+                    Console.WriteLine("Syntax Error");
+                }
+            }
+                forloop:
+                    if (tokentype[0] == forloop[0])
+                    {
+                        for (i = 0; i < forloop.Length; i++)
+                        {
+                            exist = 0;
+                            foreach (string x in tokentype)
+                            {
+                                if (forloop[i] == x)
+                                {
+                                    exist = 1;
+                                }
+                            }
+                            if (exist == 0)
+                            {
+                                Console.WriteLine("{0} is missing", forloop[i]);
+                            }
+                            if (tokentype[i] != forloop[i])
+                            {
+                                ErrorCommand = true;
+                            }
+
+                        }
+                        if (ErrorCommand == true)
+                        {
+                            Console.WriteLine("Error ");
+                        }
+                    }   
+
+                }
+            }
+            }
+    
+
---
+++ Typechecking/ConsoleApp2/dictionary.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class dictionary
+    {
+        public Dictionary<string, string> dictionary1;
+        public Dictionary<string, string> dictionary2;
+        public dictionary()
+        {
+            dictionary1 = new Dictionary<string, string>();
+
+            dictionary1.Add("+", "Addition");
+            dictionary1.Add("-", "Subtraction");
+            dictionary1.Add("*", "Multiplication");
+            dictionary1.Add("/", "Division");
+            dictionary1.Add("%", "Reminder");
+            dictionary1.Add("--", "Decrement");
+            dictionary1.Add("++", "Increment");
+            //relational
+            dictionary1.Add("==", "EqualTo");
+            dictionary1.Add("!=", "Not equal to");
+            dictionary1.Add("<", "relational operator");
+            dictionary1.Add(">", "relational operator");
+            dictionary1.Add("<=", "Lessequal");
+            dictionary1.Add(">=", "Greaterequal");
+            //Logical
+            dictionary1.Add("!", "NOT");
+            dictionary1.Add("&&", "AND");
+            dictionary1.Add("||", "OR");
+            //Bitwise
+            dictionary1.Add("^", "Bitwise XOR");
+            dictionary1.Add("|", "Bitwise OR");
+            dictionary1.Add("&", "Bitwise AND");
+            dictionary1.Add("<<", "ShiftLeft");
+            dictionary1.Add(">>", "ShiftRight");
+            dictionary1.Add(">>>", "Shiftrightzero");
+            dictionary1.Add("`", "complement");
+            //Assignment Operator
+            dictionary1.Add("=", "Equal");
+            dictionary1.Add("+=", "Add AND");
+            dictionary1.Add("-=", "Subtract AND");
+            dictionary1.Add("*=", "Multiply AND");
+            dictionary1.Add("%=", "Modulus AND");
+            dictionary1.Add("<<=", "Left shift AND");
+            dictionary1.Add(">>=", "Right shift AND");
+            dictionary1.Add("&=", "Bitwise AND");
+            dictionary1.Add("^=", "Bitwise exclusive OR");
+            dictionary1.Add("|=", "Bitwise inclusive OR");
+            dictionary1.Add("/=", "Divide AND");
+            //dataType
+            dictionary1.Add("int", "datatype");
+            dictionary1.Add("float", "datatype");
+            dictionary1.Add("char", "datatype");
+            dictionary1.Add("boolean", "datatype");
+            dictionary1.Add("long", "datatype");
+            dictionary1.Add("short", "datatype");
+            dictionary1.Add("string", "datatype");
+            dictionary1.Add("void", "this is ValueLess");
+            dictionary1.Add("wchar_t", "this is Wide character");
+            dictionary1.Add("double", "this is DOUBLE");
+            dictionary1.Add("byte", "this is BYTE");
+            dictionary1.Add("int16", "this is INTEGER16");
+            dictionary1.Add("int32", "this is INTEGER32");
+            dictionary1.Add("int64", "this is INTEGER64");
+            //keyword
+            dictionary1.Add("for","keyword");
+            dictionary1.Add("while", "keyword");
+            dictionary1.Add("Foreach", "keyword");
+            dictionary1.Add("do", "keyword");
+            dictionary1.Add("break", "keyword");
+            //brackets and symbols
+            dictionary1.Add(";", "semi colon");
+            dictionary1.Add(",", "comma");
+            dictionary1.Add("(", "opening round bracket");
+            dictionary1.Add(")", "closing round bracket");
+            dictionary1.Add("{", "opening curly bracket");
+            dictionary1.Add("}", "closing curly bracket");
+        }
+    }
+}
